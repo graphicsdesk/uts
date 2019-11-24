59cdade30e3f642115d39538e566f640df30ec9f
Jason Kao <jason.kao@columbia.edu>
2019-11-24 14:51:36 -0500

    spectate set up and ai2html

--- src/script.js
+++ src/script.js
@@ -6,6 +6,18 @@ import { USE_COVER_HED } from '../config.yml';
 // Fade in navbar at scroll trigger
 
 const navbar = document.getElementById('navbar');
+enterView({
+  selector: USE_COVER_HED ? '.headline' : '.step-deck',
+  offset: USE_COVER_HED ? 1 : 0.957,
+  enter: () => {
+    navbar.classList.remove('only-logo');
+  },
+  exit: () => {    
+    navbar.classList.remove('show-nav-links');
+    navbar.classList.add('only-logo');
+  },
+});
+
 
 // Mobile navbar hamburger trigger
 
